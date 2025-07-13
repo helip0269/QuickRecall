932b4420b500e0c03db5cc956a471248f220e0a0
Heli Patel <helip0269@gmail.com>
2025-07-13 10:45:33 -0400

    updated server.js

--- quickrecall-server/server.js
+++ quickrecall-server/server.js
@@ -10,7 +10,15 @@ const flashcardRoutes = require('./routes/flashcards');
 require('dotenv').config();
 const app = express();
 
-app.use(cors());
+
+app.use(cors({
+  origin: 'https://quickrecall-client.onrender.com', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+
+app.options('*', cors());
 app.use(express.json());
 
 
